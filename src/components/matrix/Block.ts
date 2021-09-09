35152bd9a71206cc32cda70abf39c420c11298d3
Carlos Alberto Zequeira Sanchez <rafaelyrm@xetid.cu>
2021-09-09 00:08:23 -0400

    añadido el dialog y cambiado los css

--- src/components/matrix/Block.ts
+++ src/components/matrix/Block.ts
@@ -1,15 +1,22 @@
-import { div } from 'bitterify/lib';
+import { bind, div } from 'bitterify/lib';
 import { IBlock } from '../../interfaces';
+import { dialog } from '../functions';
 
 export function Block(block: IBlock) {
+  const visible = bind(false, 'boolean');
+
   let color = 'bg-white';
   if (block.value && block.value >= 2) color = 'bg-red-600';
   if (block.value && block.value >= 3) color = 'bg-yellow-600';
   if (block.value && block.value >= 4) color = 'bg-green-600';
 
-  const item = div([block.denomination]).setClasses(`m-1 w-12 h-12 ${color}`);
+  const button = div([block.denomination]).setClasses(
+    `cursor-pointer m-1 w-12 h-12 ${color} flex justify-center items-center`,
+  );
 
-  if (block.column === block.file) item.addClasses('ring');
+  button.addEvent('click', () => (visible.value = true));
 
-  return item;
+  if (block.column === block.file) button.addClasses('ring');
+
+  return div([button, dialog(visible, block.denomination, 'Hola')]);
 }
