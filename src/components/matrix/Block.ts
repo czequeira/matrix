63c96896e7877aa73e0cbb21ab84f3805315d170
Carlos Alberto Zequeira Sanchez <rafaelyrm@xetid.cu>
2021-09-09 04:01:53 -0400

    mejorada la forma en la que se ve la tabla

--- src/components/matrix/Block.ts
+++ src/components/matrix/Block.ts
@@ -6,21 +6,25 @@ import { Info } from './Info';
 export function Block(block: IBlock) {
   const visible = bind(false, 'boolean');
 
-  let color = 'bg-white';
-  if (block.value && block.value >= 2) color = 'bg-red-600';
-  if (block.value && block.value >= 3) color = 'bg-yellow-600';
-  if (block.value && block.value >= 4) color = 'bg-green-600';
-
-  const button = div([block.denomination]).setClasses(
-    `cursor-pointer m-0.5 w-12 h-12 ${color} flex justify-center items-center`,
+  const Cell = div([block.denomination]).setClasses(
+    `cursor-pointer m-0.5 w-16 h-16 flex justify-center opacity-50 items-center hover:opacity-100`,
   );
 
-  button.addEvent('click', () => (visible.value = true));
+  if (block.value && block.value >= 4)
+    Cell.addClasses('bg-green-600 text-white');
+  else if (block.value && block.value >= 3)
+    Cell.addClasses('bg-yellow-400 text-white');
+  else if (block.value && block.value >= 2)
+    Cell.addClasses('bg-red-600 text-white');
+  else Cell.addClasses('bg-gray-100 text-gray-900');
 
-  if (block.column === block.file) button.addClasses('ring');
+  Cell.addEvent('click', () => (visible.value = true));
+
+  if (block.column === block.file)
+    Cell.removeClasses('opacity-50').addClasses('opacity-90');
 
   return div([
-    button,
+    Cell,
     dialog(visible, `${block.file}-${block.column}`, Info(block)),
   ]);
 }
