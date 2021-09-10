19f91c32a47d0e7691e052ca1c72a255ee5d296e
Carlos Alberto Zequeira Sanchez <rafaelyrm@xetid.cu>
2021-09-10 17:51:06 -0400

    puesta la evaluacion mejor

--- src/components/matrix/Info.ts
+++ src/components/matrix/Info.ts
@@ -2,7 +2,11 @@ import { div, h3 } from 'bitterify/lib';
 import { IBlock } from '../../interfaces';
 
 export function Info(block: IBlock) {
-  const value = div([h3(`${block.value}`.substr(0, 4))]);
+  const value = div([
+    h3(`${block.value}`.substr(0, 4)).setClasses(
+      'text-6xl font-bold text-gray-800',
+    ),
+  ]);
   value.setClasses('flex justify-center');
 
   const note = div([block.note || 'No hay notas']);
