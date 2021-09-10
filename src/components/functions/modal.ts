98dd25cbd678f8c1ff8b175ea8989a0a0484eb60
Carlos Alberto Zequeira Sanchez <rafaelyrm@xetid.cu>
2021-09-10 16:52:09 -0400

    mejorado el modal

--- src/components/functions/modal.ts
+++ src/components/functions/modal.ts
@@ -1,18 +1,26 @@
 import { b, div } from 'bitterify/lib';
+import { Child } from 'bitterify/lib/core/types';
 import { icon } from './icon';
 
-const Modal = div([
-  div([
+function createHeader(title: Child = '') {
+  const Title = typeof title === 'string' ? b(title) : title;
+  return div([
     div(),
-    b('Hola'),
+    Title,
     icon('M6 18L18 6M6 6l12 12', 'h-4 w-4 cursor-pointer').addEvent(
       'click',
       closeModal,
     ),
-  ]).setClasses('flex justify-between items-center mb-4'),
-]).setClasses('bg-white shadow-xl max-w-2xl w-full rounded p-2 text-gray-800');
+  ]).setClasses('flex justify-between items-center mb-4');
+}
 
-export const Overlay = div([Modal])
+function createModal(Header: Child, Body: Child = '') {
+  return div([Header, Body]).setClasses(
+    'bg-white shadow-xl max-w-2xl w-full rounded p-2 text-gray-800',
+  );
+}
+
+export const Overlay = div()
   .setClasses(
     'bg-black bg-opacity-20 absolute inset-0 hidden justify-center items-center p-4',
   )
@@ -22,6 +30,9 @@ export function closeModal() {
   Overlay.removeClasses('flex').addClasses('hidden');
 }
 
-export function modal() {
+export function modal(title?: Child, body?: Child) {
+  const Header = createHeader(title);
+  const Modal = createModal(Header, body);
   Overlay.removeClasses('hidden').addClasses('flex');
+  Overlay.setChildren([Modal]);
 }
